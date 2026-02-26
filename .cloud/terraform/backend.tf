9748fd959164f419ff73cd2feeb747380a144f71
totofile <theophilenp@gmail.com>
2026-02-26 08:53:01 +0100

    Update Terraform backend configuration to specify S3 bucket, key, region, and enable encryption for state management.

--- .cloud/terraform/backend.tf
+++ .cloud/terraform/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "training-esgi-aws"
+    key    = "terraform/terraform.tfstate"
+    region = "eu-west-3"
+    encrypt = true
+    }
 }
